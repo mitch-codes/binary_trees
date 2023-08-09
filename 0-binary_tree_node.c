94ad3495da85f9becbb61fc2f2ec4725ba1883e4
mitch codes <mitchkodes@gmail.com>
2023-08-09 10:55:30 +0000

    redoes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,7 +5,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
     new_node->n = value;
-    new_node->parent = NULL;
+    new_node->parent = parent;
     new_node->left = NULL;
     new_node->right = NULL;
     return (new_node);

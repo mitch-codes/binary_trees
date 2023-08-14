c0089b124154ce1bb0df59d0be0d3fc05c4e54a3
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-08-14 11:51:32 +0300

    assign accurate values for left and parent nodes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,15 +16,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         {
 		binary_tree_t *left_node = binary_tree_node(parent, value);
 		parent->left = left_node;
+		return (left_node);
         }
         else
         {
                 binary_tree_t *left_node = malloc(sizeof(binary_tree_t));
                 left_node->n = value;
-		left_node->parent = parent;
+                left_node->parent = parent;
                 left_node->left = parent->left;
+                parent->left->parent = left_node;
                 parent->left = left_node;
                 left_node->right = NULL;
+                return (left_node);
         }
-	return (left_node);
+	return NULL;
 }

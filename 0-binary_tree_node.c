443a5ab43af95aef18fd2ca463128e8a86a1e391
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-08-14 11:33:46 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -22,5 +22,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
+	parent->left = new_node;
 	return (new_node);
 }

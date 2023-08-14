b3c6b7012555a8407e0dc97690a6bdc630ca3f7e
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-08-14 11:55:06 +0300

    alter syntax

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,12 +15,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         if (!parent->left)
         {
 		binary_tree_t *left_node = binary_tree_node(parent, value);
+
+		if (!left_node)
+		{
+			return (NULL);
+		}
 		parent->left = left_node;
 		return (left_node);
         }
         else
         {
                 binary_tree_t *left_node = malloc(sizeof(binary_tree_t));
+
                 left_node->n = value;
                 left_node->parent = parent;
                 left_node->left = parent->left;

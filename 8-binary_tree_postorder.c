4cc7cf1cb847c9fb7290e4c67208b79cd8f7ec61
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-08-14 13:53:40 +0300

    transverse postorder left-right-root

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,20 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary
- * tree using post-order traversal
- * @tree: binary tree
- * @func:  pointer to a function to call for each node
+ *binary_tree_postorder - transverses tree postorder i.e left - right - root
+ *@tree: pointer to the root node
+ *@func: function to be carried out
  *
- * Return: Nothing.
- */
+ *Return: void
+**/
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-	{
-		return;
-	}
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+        if (tree && func)
+        {
+		binary_tree_inorder(tree->left, func);
+                binary_tree_inorder(tree->right, func);
+                func(tree);
+        }
 }

a2d405350feb655e098dff146f6fb16d4f23be3f
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-08-15 14:13:06 +0300

    make it dry

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,18 +9,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left, right;
+        size_t leaves;
 
-	left = right = 0;
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-	left = binary_tree_leaves(tree->left);
-	right = binary_tree_leaves(tree->right);
-	return (left + right);
+        if (!tree)
+        {
+                return (0);
+        }
+        if(tree->left == NULL && tree->right == NULL)
+        {
+                return (1);
+        }
+        leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+        return (leaves);
 }

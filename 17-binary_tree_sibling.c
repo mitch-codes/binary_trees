256bc3792968b4bf575a7100c3777389da13a911
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-08-18 09:47:03 +0300

    why was this easy

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,24 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - a function to finds the sibling of a node
+ * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to node to find sibling of
  *
- * Return: pointer to sibling node, else NULL
- */
+ * Return: pointer to sibling node or NULL
+ **/
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL ||
-			node->parent->left == NULL || node->parent->right == NULL)
-	{
-		return (NULL);
-	}
-	if (node->parent->left->n == node->n)
-	{
-		return (node->parent->right);
-	}
-	else
-	{
-		return (node->parent->left);
-	}
+        if (node == NULL || node->parent == NULL)
+        {
+                return NULL;
+        }
+        if (node->parent->left == NULL || node->parent->right == NULL)
+        {
+                return NULL;
+        }
+        if (node == node->parent->left)
+        {
+                return (node->parent->right);
+        }
+        else
+        {
+                return (node->parent->left);
+        }
 }

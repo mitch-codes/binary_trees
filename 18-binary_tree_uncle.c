c7874ca9447bc4bcfacc3b8bc58442eca4bb9174
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-08-18 10:02:08 +0300

    first try

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,28 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - function that finds
- * the uncle node or the node that
- * shares the same sibling as the parent node.
- * @node: the node to be evaluated
+ * binary_tree_uncle - find the uncle of a node
+ * @node: pointer to node to find sibling of
  *
- * Return: the uncle node or NULL
- */
+ * Return:the unclee node or NULL
+ **/
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-	{
-		return (NULL);
-	}
-	if (node->parent->parent->right != NULL &&
-			node->parent->parent->right != node->parent)
-	{
-		return (node->parent->parent->right);
-	}
-	else if (node->parent->parent->left != NULL &&
-			node->parent->parent->left != node->parent)
-	{
-		return (node->parent->parent->left);
-	}
-	return (NULL);
+        if (node == NULL || node->parent->parent == NULL)
+        {
+                return NULL;
+        }
+        if (node->parent->parent->left == NULL || node->parent->parent->right == NULL)
+        {
+                return NULL;
+        }
+        if (node->parent == node->parent->parent->left)
+        {
+                return (node->parent->parent->right);
+        }
+        else
+        {
+                return (node->parent->parent->left);
+        }
 }
